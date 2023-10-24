4c0f37094cc407b72b0a3f4068501d7b8132f662
Gina Medina <ga.medina0613@gmail.com>
2023-10-24 13:33:34 +0100

    Repository class with tests

---
+++ lib/User_repository.py
@@ -0,0 +1,27 @@
+from lib.User import User
+class User_repository:
+    def __init__(self, connection):
+        self._connection = connection
+
+    def all(self):
+        rows = self._connection.execute('SELECT * from USERS')
+        users = []
+        for row in rows:
+            item = User(row["id"], row["username"], row["name"], row["email"], row["password"])
+            users.append(item)
+        return users
+    
+    def add_user(self, user):
+        self._connection.execute('INSERT INTO users (username, name, email, password) VALUES (%s, %s, %s, %s)', [
+                                user.username, user.name, user.email, user.password])
+        return None
+
+    def login_valid(self, username, password):
+        rows = self._connection.execute(
+            'SELECT * from users WHERE username = %s and password = %s', [username, password])
+        if rows != []:
+            row = rows[0]
+            valid_user = User(row["id"], row["username"], row["name"], row["email"], row["password"])
+            return f"Welcome {valid_user.name}"
+        else:
+            return f"Username or password is not valid"
--- seeds/db_makers_bnb.sql
+++ seeds/db_makers_bnb.sql
@@ -1,23 +1,42 @@
 
 -- First, we must delete (drop) all our tables
-DROP TABLE IF EXISTS <USER>;
-DROP SEQUENCE IF EXISTS <table_name>_id_seq;
+-- DROP TABLE IF EXISTS <table_name>;
+-- DROP SEQUENCE IF EXISTS <table_name>_id_seq;
 
--- Then, we recreate them
-CREATE SEQUENCE IF NOT EXISTS <table_name>_id_seq;
-CREATE TABLE <table_name> (
-    id SERIAL PRIMARY KEY,
-    <column> text,
-    <column> int,
-);
+-- -- Then, we recreate them
+-- CREATE SEQUENCE IF NOT EXISTS <table_name>_id_seq;
+-- CREATE TABLE <table_name> (
+--     id SERIAL PRIMARY KEY,
+--     <column> text,
+--     <column> int,
+-- );
 
-INSERT INTO <table_name> (<columns>) VALUES (<values>);
+-- INSERT INTO <table_name> (<columns>) VALUES (<values>);
 
 -- Write file into database in terminal
 -- psql -h 127.0.0.1 MAKERS_BNB < db_makers_bnb.sql
 -- psql -h 127.0.0.1 MAKERS_BNB_TEST < db_makers_bnb.sql
 
+DROP TABLE IF EXISTS users;
+DROP SEQUENCE IF EXISTS users_id_seq;
 
+-- Then, we recreate them
+CREATE SEQUENCE IF NOT EXISTS users_id_seq;
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username text,
+    name text,
+    email text,
+    password text
+);
+
+INSERT INTO users (username, name, email, password) VALUES ('Amina_1', 'Amina', 'Amina@mail.com','Amina123!');
+INSERT INTO users (username, name, email, password) VALUES ('Jake_1', 'Jake', 'Jake@mail.com','Jake123!');
+INSERT INTO users (username, name, email, password) VALUES ('Sudhansh_1', 'Sudhansh', 'Sudhansh@mail.com','Sudhansh123!');
+
+-- Write file into database in terminal
+-- psql -h 127.0.0.1 MAKERS_BNB < db_makers_bnb.sql
+-- psql -h 127.0.0.1 MAKERS_BNB_TEST < db_makers_bnb.sql
 DROP TABLE IF EXISTS spaces;
 DROP SEQUENCE IF EXISTS spaces_id_seq;
 
