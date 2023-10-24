cd0aa3a2a5561abd94a66dcccf15fd20ba96b7a0
JakeOtto <jake_zimmermann@hotmail.co.uk>
2023-10-24 15:42:51 +0100

    added request class and repo

--- lib/request_repository.py
+++ lib/request_repository.py
@@ -8,7 +8,15 @@ class Request_repository:
     def add_request(self, request_user_id, space_id,requested_date, status):
 
         new_request = Request(request_user_id, space_id,requested_date, status)
-        return f" Added: {new_request.__repr__()}"
+        self._connection.execute('INSERT INTO requests (request_user_id, space_id, requested_date, status) VALUES (%s, %s, %s, %s)', [
+                                new_request.request_user_id, new_request.space_id, new_request.requested_date, new_request.status])
+        return None
 
-
+    def get_all_requests(self, db_connection):
+        rows = self._connection.execute('SELECT * from requests')
+        requests = []
+        for row in rows:
+            item = Request(row["id"], row["request_user_id"], row["space_id"], row["requested_date"], row["status"])
+            requests.append(item)
+        return requests
 
--- tests/test_request_repository.py
+++ tests/test_request_repository.py
@@ -9,3 +9,13 @@ def test_request_add():
     assert new_request.space_id == 1
     assert new_request.requested_date == "13/2/23"
     assert new_request.password == False
+
+
+def test_get_all_users(db_connection):
+    
+    db_connection.seed("seeds/db_makers_bnb.sql")
+    new_repository = Request_repository(db_connection)
+    
+    all_requests = new_repository.all()
+    assert all_requests == [Request(1,1,2, '12/12/23',False), \
+                            Request(2,2,3,'03/08/23',False)]
